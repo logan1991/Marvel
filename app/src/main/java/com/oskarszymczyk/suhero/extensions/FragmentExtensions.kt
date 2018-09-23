e25d32eea5e0f26afb923b17966c8bedc3273c6a
oskarszymczyk <oskarszymczyk91@gmail.com>
2018-09-23 18:28:37 +0200

    - remove comments from FragmentExtensions - remove semicolon from AuthInterceptor

--- app/src/main/java/com/oskarszymczyk/suhero/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/oskarszymczyk/suhero/extensions/FragmentExtensions.kt
@@ -7,11 +7,3 @@ import android.support.v4.app.Fragment
 
 fun <T : ViewModel> Fragment.createVieModel(viewModelFactory: ViewModelProvider.Factory, modelClass: Class<T>) =
         ViewModelProviders.of(this, viewModelFactory).get(modelClass)
-
-
-//inline fun <reified T : ViewModel> createViewModel(fragment: Fragment): T {
-//    val factory = Injector.getViewModelFactory()
-//    return ViewModelProviders.of(fragment, factory).get(T::class.java)
-//}
-//
-//inline fun <reified T : ViewModel> Fragment.viewModelDelegate() = lazy { createViewModel<T>(this) }
